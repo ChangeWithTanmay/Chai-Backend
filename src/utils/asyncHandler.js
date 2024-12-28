99013420e0af1cb492631e2bff85d3399bb34baa
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2024-12-28 20:44:17 +0530

    util code discoussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandeler = (requestHandeler) =>{
+    (req, res, next) => {
+        Promise.resolve(requestHandeler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+export { asyncHandeler }
+
+// const asyncHandeler= ()=>{}
+// const asyncHandeler = (func)=>{()=>{}}
+
+    /* 
+const asyncHandeler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message || "An unknown error occurred"
+        })
+    }
+}
+*/
