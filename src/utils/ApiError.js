bf839720b84e4be0e32cbebd21099a2786ce7e96
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2025-01-02 20:08:16 +0530

    fix temp error

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,25 +1,23 @@
 class ApiError extends Error {
   constructor(
-   statusCode,
-   message="Sonething went wrong",
-   errors=[],
-   statck = ""
-   
-  ){
-    super(message)
-    this.statusCode = statusCode
-    this.data=null
-    this.message = message
+    statusCode,
+    message = "Sonething went wrong",
+    errors = [],
+    statck = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
     this.success = false;
-    this.errors = errors
+    this.errors = errors;
 
-
-    if(statck){
-        this.stack = stack
-    }else{
-        Error.captureStackTrace(this, this.constructor);
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
   }
 }
 
-export { ApiError }
+export { ApiError };
