502531953a84cfdef7055064d7a0d530e7097b3a
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2025-02-08 21:54:04 +0530

    Solution input data not come throght  form-data.

--- src/routes/playlist.router.js
+++ src/routes/playlist.router.js
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 import { 
     creatPlayList,
@@ -9,6 +10,10 @@ import {
  router.use(verifyJWT);
 
 
- router.route("/").post(creatPlayList);
+ router.route("/").post(
+    // Use upload.none() if you are not uploading files.
+    upload.none(),
+    creatPlayList
+);
 
  export default router;
